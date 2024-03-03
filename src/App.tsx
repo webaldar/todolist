82043f3d774da3b998edc11a5917311d54aebdf5
webaldar <webaldar@gmail.com>
2024-03-03 13:54:54 +0300

    task 1.5

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import './App.css';
 import Todolist from './Todolist';
+import * as string_decoder from 'string_decoder';
 
+type taskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
 function App() {
+    const tasks1: Array<taskType> = [
+        {id: 1, title: 'HTML CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false},
+    ]
+    const tasks2: Array<taskType> = [
+        {id: 1, title: 'Wondeful', isDone: true},
+        {id: 2, title: 'My song', isDone: false},
+        {id: 3, title: 'Word of Tunder', isDone: false},
+    ]
     return (
         <div className="App">
-            <Todolist title={'What to learn'}/>
-            <Todolist title={'Song'}/>
-            <Todolist title={'Book'}/>
+            <Todolist title={'What to learn'} task={tasks1}/>
+            <Todolist title={'Song'} task={tasks2}/>
+
         </div>
     );
 }
